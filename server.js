0698c3cea84dc038a53e8349a7aef2b614f77760
Summer <user@users-MacBook-Air.local>
2022-06-11 15:30:19 -0400

    change socket origin

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3002",
+    origin: "https://delvetest.herokuapp.com/",
     credentials: true,
     methods: ["GET", "POST"],
   }
